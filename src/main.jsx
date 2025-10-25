a9956d027a877dd90fa1a6e432653825d9ee9ec2
Manikandan P <manikandan.aeroeng@gmail.com>
2025-10-25 18:45:21 +0530

    Replace App component with VisitorForm

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import VisitorForm from './VisitorForm'  // ✅ import your visitor page
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <VisitorForm />   {/* ✅ this loads your visitor form first */}
+  </React.StrictMode>
 )
