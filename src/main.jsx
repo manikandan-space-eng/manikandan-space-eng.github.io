60fe08057fd69f7a45fd06400ba0e57af691bd23
Manikandan P <manikandan.aeroeng@gmail.com>
2025-10-25 19:06:47 +0530

    Update VisitorForm import and render comment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import VisitorForm from './VisitorForm'  // ✅ import your visitor page
+import VisitorForm from './VisitorForm'   // ✅ make sure this line exists
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <VisitorForm />   {/* ✅ this loads your visitor form first */}
+    <VisitorForm />   {/* ✅ this should load first */}
   </React.StrictMode>
 )
